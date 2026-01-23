373d304aaf4bd758382602ae76bac0bc52f0ac4d
Jaume <jaumeni@gmail.com>
2026-01-23 09:51:23 +0100

    fix layout and claryti integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,3 @@
-import Link from "next/link";
-import Script from "next/script";
-
 import "./globals.css";
 import Script from "next/script";
 
@@ -16,25 +13,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-
-      <Script
-        id="clarity-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "v5uz6y7t6t");
-          `,
-        }}
-      />
-
       <body>
-        {children}
-      </body>
 
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "v5uz6y7t6t");
+            `,
+          }}
+        />
+
+        {children}
+
+      </body>
     </html>
   );
 }
